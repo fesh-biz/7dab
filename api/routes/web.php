7a07bc29d5271d7dcab5987b8af30de36b0a7ba8
Felix Shelishkevich <feshbiz@gmail.com>
2023-01-25 21:32:31 +0200

    Temporary closed

--- api/routes/web.php
+++ api/routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return view('quasar');
-})->where('any', '.*');
+})
+    ->middleware(['auth.basic'])
+    ->where('any', '.*');
