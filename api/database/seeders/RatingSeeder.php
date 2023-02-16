c2e14db99df919ccbb6474ee3395df4048e577f9
Felix Shelishkevich <feshbiz@gmail.com>
2023-02-16 16:41:40 +0200

    Rating system: rating seeder

--- api/database/seeders/RatingSeeder.php
+++ api/database/seeders/RatingSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Content\Post;
+use App\Models\Rating\Rating;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class RatingSeeder extends Seeder
@@ -13,6 +16,18 @@ class RatingSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (User::all() as $user) {
+            Rating::factory()->create([
+                'ratingable_id' => $user->id,
+                'ratingable_type' => User::class
+            ]);
+        }
+        
+        foreach (Post::all() as $post) {
+            Rating::factory()->create([
+                'ratingable_id' => $post->id,
+                'ratingable_type' => Post::class
+            ]);
+        }
     }
 }
