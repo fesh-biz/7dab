cc85c52fa395cdfa3bdc384597a6c84baea36156
Felix Shelishkevich <feshbiz@gmail.com>
2023-02-26 16:17:41 +0200

    Build

--- client/src/components/content/Post.vue
+++ client/src/components/content/Post.vue
@@ -123,6 +123,7 @@ export default {
       console.log('post image loaded', this.post.is_images_loaded)
       console.log('post image is expanded', this.post.is_expanded)
       console.log('is post page', this.isPostPage)
+      console.log('post', Post.query().find(this.post.id))
       console.log('--------------------------')
     }
   },
