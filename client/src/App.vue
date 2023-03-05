b982fc937ea08fb14f8f438aeb08321c39362555
Felix Shelishkevich <feshbiz@gmail.com>
2023-03-06 01:59:56 +0200

    removed cl

--- client/src/App.vue
+++ client/src/App.vue
@@ -22,7 +22,6 @@ export default {
 
   watch: {
     '$route' (toRoute, fromRoute) {
-      console.log('routeMetaTitle', this.$route.meta.title)
       if (this.$route.meta && this.$route.meta.title) {
         window.document.title = `${this.$t(this.$route.meta.title)} - ${this.$t('terevenky')}`
       } else {
