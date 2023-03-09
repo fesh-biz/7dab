f5fd7fcef8d1d9dcc00aa523e714ec1f6f9a136b
Felix Shelishkevich <feshbiz@gmail.com>
2023-03-09 12:28:16 +0200

    Deleted cache from App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,7 +8,6 @@ import MeCookies from 'src/plugins/cookies/me'
 import Token from 'src/plugins/cookies/token'
 import UserApi from 'src/plugins/api/user'
 import Me from 'src/models/user/me'
-import Cache from 'src/plugins/cache/cache'
 
 export default {
   name: 'App',
@@ -17,14 +16,12 @@ export default {
     return {
       tokenCookies: new Token(),
       userApi: new UserApi(),
-      meCookies: new MeCookies(),
-      cache: new Cache()
+      meCookies: new MeCookies()
     }
   },
 
   watch: {
     '$route' (toRoute, fromRoute) {
-      this.cache.refreshCache()
       if (this.$route.meta && this.$route.meta.title) {
         window.document.title = `${this.$t(this.$route.meta.title)} - ${this.$t('terevenky')}`
       } else {
