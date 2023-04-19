5b417f6a92de823cac96b7d1edf32b252b5482b0
Felix Shelishkevich <feshbiz@gmail.com>
2023-04-19 18:25:21 +0300

    Profile: CommentsTab.vue

--- client/src/pages/profile/Index.vue
+++ client/src/pages/profile/Index.vue
@@ -44,65 +44,7 @@
 
             <!-- Comments -->
             <q-tab-panel name="comments">
-              <data-table
-                no-top
-                url="profile/comments"
-                :columns="commentTableColumns"
-                row-key="comment"
-                use-body
-              >
-                <template #table="props">
-                  <q-tr>
-                    <!-- Actions -->
-                    <q-td>
-                      <!-- Preview -->
-                      <q-item
-                        :to="{name: 'postPage', params: {id: props.props.row.post_id}, query: {c: props.props.row.id}}"
-                        target="_blank"
-                      >
-                        <q-item-section>
-                          <icon-with-tooltip
-                            :tooltip="$t('preview')"
-                            color="positive"
-                            size="sm"
-                            icon="pageview"
-                          />
-                        </q-item-section>
-                      </q-item>
-                    </q-td>
-
-                    <!-- Reply On -->
-                    <q-td>
-                      <icon-with-tooltip
-                        size="sm"
-                        :icon="getReplyOnIcon(props.props.row.commentable_type_name)"
-                        :tooltip="getReplyOnTooltip(props.props.row.commentable_type_name)"
-                        color="grey-8"
-                      />
-                    </q-td>
-
-                    <!-- Content -->
-                    <q-td>
-                      {{ getShortString(props.props.row.body, 30) }}
-                    </q-td>
-
-                    <!-- Post Title -->
-                    <q-td>
-                      {{ getShortString(props.props.row.post.title, 30) }}
-                    </q-td>
-
-                    <!-- Rating -->
-                    <q-td>
-                      {{ getRating(props.props.row.rating) }}
-                    </q-td>
-
-                    <!-- Total Answers -->
-                    <q-td>
-                      {{ props.props.row.clean_answers_count }}
-                    </q-td>
-                  </q-tr>
-                </template>
-              </data-table>
+              <comments-tab />
             </q-tab-panel>
 
             <q-tab-panel name="answers">
@@ -121,13 +63,12 @@ import Stats from 'components/user/Stats'
 import PostsTabs from 'components/profile/PostsTabs'
 import Me from 'src/models/user/me'
 import ProfileApi from 'src/plugins/api/profile'
-import DataTable from 'components/common/DataTable'
-import IconWithTooltip from 'components/common/IconWithTooltip'
+import CommentsTab from 'components/profile/CommentsTab'
 
 export default {
   name: 'Account',
 
-  components: { Stats, PostsTabs, DataTable, IconWithTooltip },
+  components: { CommentsTab, Stats, PostsTabs },
 
   data () {
     return {
@@ -136,45 +77,7 @@ export default {
       isFetchingStats: true,
       api: new ProfileApi(),
       contentStats: null,
-      statsFetched: false,
-      commentTableColumns: [
-        {
-          required: true,
-          label: this.$t('actions'),
-          align: 'left',
-          sortable: false
-        },
-        {
-          required: true,
-          label: this.$t('reply_on'),
-          align: 'left',
-          sortable: false
-        },
-        {
-          required: true,
-          label: this.$t('comment'),
-          align: 'left',
-          sortable: false
-        },
-        {
-          required: true,
-          label: this.$t('post'),
-          align: 'left',
-          sortable: false
-        },
-        {
-          required: true,
-          label: this.$t('rating'),
-          align: 'left',
-          sortable: false
-        },
-        {
-          required: true,
-          label: this.$t('answers'),
-          align: 'left',
-          sortable: false
-        }
-      ]
+      statsFetched: false
     }
   },
 
@@ -184,39 +87,6 @@ export default {
       const res = await this.api.fetchContentStats()
       this.isFetchingStats = false
       this.contentStats = res.data
-    },
-
-    getReplyOnIcon (commentableType) {
-      if (commentableType === 'posts') {
-        return 'mms'
-      }
-
-      return 'mode_comment'
-    },
-
-    getReplyOnTooltip (commentableType) {
-      if (commentableType === 'posts') {
-        return this.$t('post')
-      }
-
-      return this.$t('comment')
-    },
-
-    getShortString (str, chars) {
-      let res = str.substring(0, 30)
-      if (str.length > chars) {
-        res += '...'
-      }
-
-      return res
-    },
-
-    getRating (rating) {
-      const p = rating.positive_votes
-      const n = rating.negative_votes
-      const t = p - n
-
-      return `${t} (${p} - ${n})`
     }
   }
 }
