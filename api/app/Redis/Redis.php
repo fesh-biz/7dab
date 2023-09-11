0d9b5cb39040cbcb1af1c78846ba80c2c2f336a4
Felix Shelishkevich <feshbiz@gmail.com>
2023-09-11 10:25:44 +0300

    Redis: code style

--- api/app/Redis/Redis.php
+++ api/app/Redis/Redis.php
@@ -8,73 +8,73 @@ class Redis
 {
     protected Client $client;
     protected string $key;
-    
+
     public function __construct(Client $client)
     {
         $this->client = $client;
-        
+
         $this->key = $this->getKey();
     }
-    
+
     public function search(string $key, $needle): array
     {
         $all = $this->all();
-        
+
         $res = [];
         foreach ($all as $id => $obj) {
             if ($obj->{$key} === $needle) {
                 $res[$id] = $obj;
             }
         }
-        
+
         return $res;
     }
-    
+
     public function create(int $id, array $data)
     {
         $data = json_encode($data);
-        
+
         $this->client->hset($this->key, $id, $data);
     }
-    
+
     public function all(): array
     {
         $res = $this->client->hgetall($this->key);
-        
+
         foreach ($res as $id => $data) {
             $res[$id] = json_decode($data) ?? $data;
         }
-        
+
         return $res;
     }
-    
+
     public function delete(int $id)
     {
         $this->client->hdel($this->key, [$id]);
     }
-    
+
     public function deleteAll()
     {
         $ids = [];
         foreach ($this->all() as $id => $data) {
             $ids[] = $id;
         }
-        
+
         $this->deleteMultiple($ids);
     }
-    
+
     public function deleteMultiple(array $ids)
     {
         $this->client->hdel($this->key, $ids);
     }
-    
+
     public function find(int $id)
     {
         $res = $this->client->hget($this->key, $id);
-        
+
         return json_decode($res) ?? $res;
     }
-    
+
     public function getKey(): string
     {
         $className = basename(str_replace('\\', '/', static::class));
