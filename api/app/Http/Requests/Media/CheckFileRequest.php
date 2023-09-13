0e1853b31bf3760f95e13c244798b9c00c697e60
Felix Shelishkevich <feshbiz@gmail.com>
2023-09-13 17:10:59 +0300

    CheckFileRequest refactoring

--- api/app/Http/Requests/Media/CheckFileRequest.php
+++ api/app/Http/Requests/Media/CheckFileRequest.php
@@ -28,7 +28,8 @@ class CheckFileRequest extends FormRequest
             throw new \Exception('The given file chunk is too large');
         }
 
-        $this->mimeType = $this->checkMimeType($file);
+        $this->mimeType = $this->getFileType($file);
+        $this->checkMimeType($this->mimeType);
 
         $this->checkFileSize($this->mimeType, $this->size);
 
@@ -47,60 +48,43 @@ class CheckFileRequest extends FormRequest
         );
     }
 
-    private function checkFileSize(string $mimeType, int $size): bool
+    private function checkFileSize(string $mimeType, int $size): void
     {
         $Mb = 1024 * 1024;
         $maxImageSize = 5 * $Mb;
         $maxGifSize = 50 * $Mb;
-        $maxVideoSize = 10 * $Mb;
+        $maxVideoSize = 100 * $Mb;
 
-        $isFileLarge = false;
-
-        $maxAllowedSize = 0;
-        switch ($mimeType) {
-            case 'image/jpeg':
-            case 'image/png':
-            case 'image/webp':
-                $maxAllowedSize = $maxImageSize;
-                break;
-            case 'image/gif':
-                $maxAllowedSize = $maxGifSize;
-                break;
-            case 'video/mp4':
-            case 'video/webm':
-                $maxAllowedSize = $maxVideoSize;
-                break;
-            default:
-                throw new \Exception("Unknown mime type: ${mimeType}");
-        }
+        $maxAllowedSize = match ($mimeType) {
+            'image/jpeg', 'image/png', 'image/webp' => $maxImageSize,
+            'image/gif' => $maxGifSize,
+            'video/mp4', 'video/webm' => $maxVideoSize,
+            default => throw new \Exception("Unknown mime type: ${mimeType}"),
+        };
 
         if ($size > $maxAllowedSize) {
             $this->throwFileValidationException(trans('validation.file_is_too_large'));
         }
-
-        ddh([
-            'mime' => $mimeType,
-            'size' => $size
-        ]);
     }
 
-    private function checkMimeType($file): string
+    private function getFileType($file): string
+    {
+        $mimeDetector = new MimeDetector();
+        $mimeDetector->setFile($file);
+        return $mimeDetector->getMimeType();
+    }
+
+    private function checkMimeType(string $mimeType): void
     {
         $imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
         $videoMimeTypes = ['video/mp4', 'video/webm'];
 
-        $mimeDetector = new MimeDetector();
-        $mimeDetector->setFile($file);
-        $mimeType = $mimeDetector->getMimeType();
-
         if (!in_array($mimeType, $imageMimeTypes) && !in_array($mimeType, $videoMimeTypes)) {
             $this->throwFileValidationException(trans('validation.wrong_file_type'));
         }
-
-        return $mimeType;
     }
 
-    private function throwFileValidationException(string $message)
+    private function throwFileValidationException(string $message): void
     {
         throw ValidationException::withMessages(['file_chunk' => $message]);
     }
